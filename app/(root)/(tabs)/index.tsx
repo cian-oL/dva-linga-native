d4295272d5a03d81a21b11e9d0e2ab1c333dbe7d
cian-oL <cianoleary2010@gmail.com>
2025-02-03 21:19:32 +0000

    wip

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/sign-in">Explore</Link>
+      <Link href="/sign-in">Profile</Link>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Image src="dva-linga-logo.png" />
+    </View>
+  );
+};
+
+export default SignIn;
